67b9363fcd33fefcdf31a503cb8214cd1366e867
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2020-08-23 12:53:15 +0300

    local save
    you can now opt to save your game to a local text file instead of localstorage, if you wish

--- resources/code/saveLoad.js
+++ resources/code/saveLoad.js
@@ -1,21 +1,31 @@
 function Saving() {
   HideAll();
   Element("saveLoad").textContent = "";
-  for(let slot of Saves) {
-    if(slot.key == "back") {
+  for (let slot of Saves) {
+    if (slot.key == "back") {
       let button = Create("button");
       button.textContent = `Back`;
       button.style.fontSize = "48px";
+      button.style.width = "260px";
       Element("saveLoad").appendChild(button);
-      button.addEventListener("click", ()=>toggleButton("settings"));
+      button.addEventListener("click", () => toggleButton("settings"));
     }
-    else if(!slot.save) {
+    else if (slot.key == "file") {
+      let button = Create("button");
+      button.textContent = `Save to File`;
+      button.style.width = "160px";
+      button.style.fontSize = "24px";
+      button.style.height = "90px";
+      Element("saveLoad").appendChild(button);
+      button.addEventListener("click", () => SaveNoPrompt("toFile"));
+    }
+    else if (!slot.save) {
       let button = Create("button");
       button.textContent = `Save ${slot.key} - empty`;
       button.style.fontSize = "24px";
       button.style.width = "500px";
       Element("saveLoad").appendChild(button);
-      button.addEventListener("click", ()=>SaveNoPrompt(slot.key));
+      button.addEventListener("click", () => SaveNoPrompt(slot.key));
     }
     else {
       let button = Create("button");
@@ -24,7 +34,7 @@ function Saving() {
       button.style.fontSize = "24px";
       button.style.width = "500px";
       Element("saveLoad").appendChild(button);
-      button.addEventListener("click", ()=>SavePrompt(slot.key));
+      button.addEventListener("click", () => SavePrompt(slot.key));
     }
   }
 }
@@ -32,15 +42,25 @@ function Saving() {
 function Loading() {
   HideAll();
   Element("saveLoad").textContent = "";
-  for(let slot of Saves) {
-    if(slot.key == "back") {
+  for (let slot of Saves) {
+    if (slot.key == "back") {
       let button = Create("button");
       button.textContent = `Back`;
       button.style.fontSize = "48px";
+      button.style.width = "260px";
       Element("saveLoad").appendChild(button);
-      button.addEventListener("click", ()=>toggleButton("settings"));
+      button.addEventListener("click", () => toggleButton("settings"));
     }
-    else if(!slot.save) {
+    else if (slot.key == "file") {
+      let button = Create("button");
+      button.textContent = `Load from File`;
+      button.style.width = "160px";
+      button.style.fontSize = "36px";
+      button.style.height = "120px";
+      Element("saveLoad").appendChild(button);
+      button.addEventListener("click", () => LoadSlotNoPrompt("toFile"));
+    }
+    else if (!slot.save) {
       let button = Create("button");
       button.textContent = `Save ${slot.key} - empty`;
       button.style.fontSize = "24px";
@@ -54,19 +74,38 @@ function Loading() {
       button.style.fontSize = "24px";
       button.style.width = "500px";
       Element("saveLoad").appendChild(button);
-      button.addEventListener("click", ()=>LoadSlotPrompt(slot.key));
+      button.addEventListener("click", () => LoadSlotPrompt(slot.key));
     }
   }
 }
 
 function SaveNoPrompt(key) {
   hideAlert();
+  if (key == "toFile") {
+    var saveData = (function () {
+      var a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+      return function (data, fileName) {
+        var json = JSON.stringify(data),
+          blob = new Blob([json], { type: "octet/stream" }),
+          url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+      };
+
+    }());
+    saveData(characters, `TOTE-${characters.player.name}-save-${new Date().getHours()}.${new Date().getMinutes()}.txt`);
+    return
+  }
   let int = 0;
-  for(let i = 0; i<Saves.length; i++) {
-    if(Saves[i].key == key) int = i;
+  for (let i = 0; i < Saves.length; i++) {
+    if (Saves[i].key == key) int = i;
   }
   Saves[int].save = true;
-  Saves[int].date = `${new Date().getDate()}.${new Date().getMonth()+1}.${new Date().getFullYear()} | ${new Date().getHours()}:${new Date().getMinutes()}`
+  Saves[int].date = `${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()} | ${new Date().getHours()}:${new Date().getMinutes()}`;
   localStorage.setItem("saves", JSON.stringify(Saves));
   localStorage.setItem(`${Saves[int].key}characters`, JSON.stringify(characters));
   Saving();
@@ -74,23 +113,56 @@ function SaveNoPrompt(key) {
 
 function LoadSlotNoPrompt(key) {
   hideAlert();
+  if (key == "toFile") {
+    let fileInput = Create("input");
+    fileInput.setAttribute('type', 'file');
+    document.body.appendChild(fileInput);
+    fileInput.id = "fileinput";
+    fileInput.click();
+    fileInput.addEventListener("change", () => HandleFile());
+    return;
+  }
   characters = JSON.parse(localStorage.getItem(`${key}characters`));
   DrawMap();
   Loading();
+}
+
+function HandleFile() {
+  console.log(Element("fileinput").files);
+  let file = Element("fileinput").files[0];
+  let reader = new FileReader();
+  let text = "";
+
+  // file reading finished successfully
+  reader.addEventListener('load', function (e) {
+    // contents of file in variable     
+    text = e.target.result;
+    FinishRead();
+  });
+
+  // read as text file
+  reader.readAsText(file);
+
+  function FinishRead() {
+    let table = JSON.parse(text);
+    characters = table;
+    DrawMap();
+    Loading();
+  }
 }
 
 function LoadSlotPrompt(key) {
   Element("AlertBase").style.display = "block";
   Element("yesAlert").textContent = "Load";
   Element("textAlert").textContent = `Are you sure you wish to load game on ${key}? You will lose all unsaved progress.`;
-  Element("yesAlert").onclick = ()=>LoadSlotNoPrompt(key);
+  Element("yesAlert").onclick = () => LoadSlotNoPrompt(key);
 }
 
 function SavePrompt(key) {
   Element("AlertBase").style.display = "block";
   Element("yesAlert").textContent = "Save";
   Element("textAlert").textContent = `Save ${key} already has a save created before. Overwrite game on save ${key}?`;
-  Element("yesAlert").onclick = ()=>SaveNoPrompt(key);
+  Element("yesAlert").onclick = () => SaveNoPrompt(key);
 }
 
 function hideAlert() {
@@ -120,15 +192,18 @@ var Saves = [
   },
   {
     key: "back"
+  },
+  {
+    key: "file"
   }
 ]
 
-if(localStorage.getItem("saves")) {
+if (localStorage.getItem("saves")) {
   Saves = JSON.parse(localStorage.getItem("saves"));
 }
 
 function RemoveStorage() {
-  for(let save of Saves) {
+  for (let save of Saves) {
     save.save = false;
   }
 }
