5e886b2fbec5aece88369e9c1bbb10a9900c14ee
Valo <69501065+disgraceofahumanbeing@users.noreply.github.com>
2020-09-07 15:53:05 +0300

    classshit

--- resources/storage/classes.js
+++ resources/storage/classes.js
@@ -18,6 +18,36 @@ const Classes = [
         desc: "yes this is desc idk",
         baseStats: new Stats(3, 3, 1, 1, 1, 1, 1, 1),
         bonusStats: {hp: 50, mana: 0}
+    },
+    {
+        key: "Healer",
+        desc: "rgfgs",
+        baseStats: new Stats(1,1,1,1,3,3,3,1,1),
+        bonusStats: {hp: 20, mana: 30}
+    },
+    {
+        key: "Rogue",
+        desc: "emo",
+        baseStats: new Stats(1,1,3,3,1,1,1,1,1),
+        bonusStats: {hp:30, mana:20 }
+    },
+    {
+        key: "Ranger",
+        desc: "uwu",
+        baseStats: new Stats(1,1,1,3,1,1,1,3,3),
+        bonusStats: {hp: 40, mana: 10}
+    },
+    {
+        key: "Brawler",
+        desc: "heh lol piplip juo paljon",
+        baseStats: new Stats(3,3,0,2,0,0,0,0,3),
+        bonusStats: {hp: 75, mana: 5}
+    },
+    {
+        key: "Necromancer",
+        desc: "lol",
+        baseStats: new Stats(1,1,3,1,3,3,0,1,1),
+        bonusStats: {hp: 20, mana: 40}
     }
 ]
 
